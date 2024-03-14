9ce52fc304a328a4a9fb83b42284beffc79d94e3
tom chaumette <chaumettetom@gmail.com>
2024-03-14 10:41:39 +0100

    ajout de la partie connection'

--- app/public/views/user/profile.php
+++ app/public/views/user/profile.php
@@ -12,10 +12,15 @@ session_start();
     <title>Profil</title>
 </head>
 <body>
+    <?php if (isset($_SESSION['user_firstname'])) { ?>
 
     <h1>Profil</h1>
     <p>Bonjour <?= $_SESSION['user_firstname'] . ' ' . $_SESSION['user_lastname'] ?></p>
     <p>Votre email est <?= $_SESSION['user_email'] ?></p>
     <a href="logout.php">Se déconnecter</a>
+    <?php } else { ?>
+    <p>Vous n'êtes pas connecté</p>
+    <a href="formConnection.php">Se connecter</a>
+    <?php } ?>
 </body>
 </html>
